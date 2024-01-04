64bc4bbd1dc5773726d08917eb7e3e7d896b9494
Sergio Ontiveros Perez <sergio.ontiveros.perez@outlook.com>
2024-01-03 19:03:42 -0500

    wip section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './Shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +7,10 @@ import { Product } from './Shared/models/product';
 })
 export class AppComponent implements OnInit {
   title = 'SkiNet';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pagesize=50').subscribe({
-      next: (response: any) => this.products = response.data, // what to do next?
-      error: error => console.log(error),
-      complete: () => {
-        console.log('request completed');
-        console.log('extra statement');
-      }
-    })
+    
   }
 }
